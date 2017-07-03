bc835cfd1dc161f8840cc67b982f12a5381f9c30
BuildTools <bkirkpa4@uncc.edu>
2017-07-02 23:54:49 -0400

    added basic hud functionality

--- core/src/Scenes/Hud.java
+++ core/src/Scenes/Hud.java
@@ -1,9 +1,12 @@
 package Scenes;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.brik.mariobros.MarioBros;
@@ -23,7 +26,7 @@ public class Hud {
     private float timeCount;
     private Integer score;
 
-    Label countdownLabel, scoreLabel, timeLabel, levelLabel, worldLabel, marioLabel;
+    private Label countdownLabel, scoreLabel, timeLabel, levelLabel, worldLabel, marioLabel;
 
     public Hud(SpriteBatch sb) {
         worldTimer = 300;
@@ -31,6 +34,30 @@ public class Hud {
         score = 0;
         viewport = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, new OrthographicCamera());
         stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+
+        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE)); //3 digits
+        scoreLabel = new Label(String.format("%06d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE)); //6 digits
+        timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        //top row
+        table.add(marioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countdownLabel).expandX();
+
+        stage.addActor(table);
     }
 
 }
